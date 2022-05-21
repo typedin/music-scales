7fa80b15441b0c7532c516c94121da32f5ed5e49
Antoine Cheynet <antoine@typedin.dev>
2022-05-21 17:56:02 +0200

    use helper to get alteration

--- src/interval-builders/MinorThird.ts
+++ src/interval-builders/MinorThird.ts
@@ -1,23 +1,10 @@
 import { Note } from "../types";
-import { index } from "../helpers";
+import { getPreviousAlteration, index } from "../helpers";
 import { diatonicNoteNames } from "../MusicalConstants";
-import { alterations } from "../../src/MusicalConstants";
-
-const { flat, doubleFlat, sharp, doubleSharp, natural } = alterations;
-function getAlteration(note: Note): string {
-  if (note.alteration == sharp) {
-    return natural;
-  }
-
-  if (note.alteration == flat) {
-    return doubleFlat;
-  }
-  return flat;
-}
 
 export default function(note: Note): Note {
   const name = diatonicNoteNames[index(note, 4)];
-  let alteration = getAlteration(note);
+  let alteration = getPreviousAlteration(note.alteration);
 
   if (name == "-") {
     return {
