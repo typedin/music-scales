5f900bd9d924f769a96a724cb163248a2bb3ba4a
Antoine Cheynet <antoine@typedin.dev>
2022-05-22 18:58:47 +0200

    can augment intervals

---
+++ src/interval-builders/Augment.ts
@@ -0,0 +1,9 @@
+import { getNextAlteration } from "../helpers";
+import { Note } from "../types";
+
+const Augment = (note: Note): Note => ({
+  ...note,
+  alteration: getNextAlteration(note.alteration),
+});
+
+export default Augment;
---
+++ tests/interval-builders/Augment.test.ts
@@ -0,0 +1,48 @@
+import Augment from "../../src/interval-builders/Augment";
+import MajorThird from "../../src/interval-builders/MajorThird";
+import PerfectFifth from "../../src/interval-builders/PerfectFifth";
+import PerfectForth from "../../src/interval-builders/PerfectForth";
+import { AlterationEnum, DiatonicNoteEnum } from "../../src/types";
+
+describe("Augment", () => {
+  it("can augment a major third", () => {
+    expect(
+      Augment(
+        MajorThird({
+          name: DiatonicNoteEnum.C,
+          alteration: AlterationEnum.natural,
+        })
+      )
+    ).toEqual({
+      name: DiatonicNoteEnum.E,
+      alteration: AlterationEnum.sharp,
+    });
+  });
+  it("can augment a perfect forth", () => {
+    expect(
+      Augment(
+        PerfectForth({
+          name: DiatonicNoteEnum.C,
+          alteration: AlterationEnum.natural,
+        })
+      )
+    ).toEqual({
+      name: DiatonicNoteEnum.F,
+      alteration: AlterationEnum.sharp,
+    });
+  });
+
+  it("can augment a perfect fifth", () => {
+    expect(
+      Augment(
+        PerfectFifth({
+          name: DiatonicNoteEnum.C,
+          alteration: AlterationEnum.natural,
+        })
+      )
+    ).toEqual({
+      name: DiatonicNoteEnum.G,
+      alteration: AlterationEnum.sharp,
+    });
+  });
+});
