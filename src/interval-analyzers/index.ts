9d44b09ae4be15bd59bd2802db7ba421ce6ce36e
Antoine Cheynet <antoine@typedin.dev>
2022-06-13 17:54:45 +0200

    can analyze basic intervals

--- src/interval-analyzers/index.ts
+++ src/interval-analyzers/index.ts
@@ -1,11 +1,96 @@
 import { Note } from "../../types";
 import is from "../interval-checkers/index";
 
-export default function (firstnote: Note, secondNote: Note) {
+type Interval = {
+  name: string;
+  quality?: string;
+};
+
+export default function (firstnote: Note, secondNote: Note): Interval | never {
   if (is("Unison", firstnote, secondNote)) {
-    return "Unison";
+    return {
+      name: "unison",
+    };
   }
+
+  if (is("MinorSecond", firstnote, secondNote)) {
+    return {
+      name: "second",
+      quality: "minor",
+    };
+  }
+
   if (is("MajorSecond", firstnote, secondNote)) {
-    return "MajorSecond";
+    return {
+      name: "second",
+      quality: "major",
+    };
   }
+
+  if (is("MinorThird", firstnote, secondNote)) {
+    return {
+      name: "third",
+      quality: "minor",
+    };
+  }
+
+  if (is("MajorThird", firstnote, secondNote)) {
+    return {
+      name: "third",
+      quality: "major",
+    };
+  }
+
+  if (is("PerfectForth", firstnote, secondNote)) {
+    return {
+      name: "forth",
+      quality: "perfect",
+    };
+  }
+
+  if (is("PerfectFifth", firstnote, secondNote)) {
+    return {
+      name: "fifth",
+      quality: "perfect",
+    };
+  }
+
+  if (is("MinorSixth", firstnote, secondNote)) {
+    return {
+      name: "sixth",
+      quality: "minor",
+    };
+  }
+
+  if (is("MajorSixth", firstnote, secondNote)) {
+    return {
+      name: "sixth",
+      quality: "major",
+    };
+  }
+
+  if (is("MinorSeventh", firstnote, secondNote)) {
+    return {
+      name: "seventh",
+      quality: "minor",
+    };
+  }
+
+  if (is("MajorSeventh", firstnote, secondNote)) {
+    return {
+      name: "seventh",
+      quality: "major",
+    };
+  }
+
+  if (is("Octave", firstnote, secondNote)) {
+    return {
+      name: "octave",
+      quality: "perfect",
+    };
+  }
+
+  throw Error(
+    `Could not check the interval between those notes: ${firstnote.name} - ${secondNote.name}`
+  );
 }
--- tests/interval-analyzers/index.test.ts
+++ tests/interval-analyzers/index.test.ts
@@ -1,23 +1,148 @@
 import is from "../../src/interval-analyzers/index";
 import { AlterationEnum, DiatonicNoteEnum, Note } from "../../types";
 
-const C4: Note = {
+const from: Note = {
   name: DiatonicNoteEnum.C,
   alteration: AlterationEnum.natural,
   octave: 4,
 };
 
 describe("index", () => {
-  it("is an unison", () => {
-    expect(is(C4, C4)).toBe("Unison");
+  it("is an Unison", () => {
+    expect(is(from, from)).toEqual({
+      name: "unison",
+    });
+  });
+
+  it("is a MinorSecond", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.D,
+      alteration: AlterationEnum.flat,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "second",
+      quality: "minor",
+    });
   });
 
   it("is a MajorSecond", () => {
-    const D4: Note = {
+    const to: Note = {
       name: DiatonicNoteEnum.D,
       alteration: AlterationEnum.natural,
       octave: 4,
     };
-    expect(is(C4, D4)).toBe("MajorSecond");
+    expect(is(from, to)).toEqual({
+      name: "second",
+      quality: "major",
+    });
+  });
+
+  it("is a MinorThird", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.E,
+      alteration: AlterationEnum.flat,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "third",
+      quality: "minor",
+    });
+  });
+
+  it("is a MajorThird", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.E,
+      alteration: AlterationEnum.natural,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "third",
+      quality: "major",
+    });
+  });
+
+  it("is a PerfectForth", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.F,
+      alteration: AlterationEnum.natural,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "forth",
+      quality: "perfect",
+    });
+  });
+
+  it("is a PerfectFifth", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.G,
+      alteration: AlterationEnum.natural,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "fifth",
+      quality: "perfect",
+    });
+  });
+
+  it("is a MinorSixth", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.A,
+      alteration: AlterationEnum.flat,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "sixth",
+      quality: "minor",
+    });
+  });
+
+  it("is a MajorSixth", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.A,
+      alteration: AlterationEnum.natural,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "sixth",
+      quality: "major",
+    });
+  });
+
+  it("is a MinorSeventh", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.B,
+      alteration: AlterationEnum.flat,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "seventh",
+      quality: "minor",
+    });
+  });
+
+  it("is a MajorSeventh", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.B,
+      alteration: AlterationEnum.natural,
+      octave: 4,
+    };
+    expect(is(from, to)).toEqual({
+      name: "seventh",
+      quality: "major",
+    });
+  });
+
+  it("is an octave", () => {
+    const to: Note = {
+      name: DiatonicNoteEnum.C,
+      alteration: AlterationEnum.natural,
+      octave: 5,
+    };
+    expect(is(from, to)).toEqual({
+      name: "octave",
+      quality: "perfect",
+    });
   });
 });
